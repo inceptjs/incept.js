ba0b929c5c1b58c67228573793c00ccc0cc07e55
Christian Blanquera <chris@incept.asia>
2021-10-15 09:52:18 +0800

    removing sandbox

--- packages/sandbox/plugin/api/index.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'inceptjs'
-
-const router = new Router
-
-router.get('/api/rest', (req, res) => {
-  res.body = { error: false }
-}, 10)
-
-export default router
--- packages/sandbox/plugin/www/page.js
+++
@@ -1,5 +0,0 @@
-export default function(react) {
-  const page = react.makePage()
-  page.title = 'Welcome to Incept'
-  return page
-}
