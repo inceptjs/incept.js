7915bd97a93550068bfc0d4722d028b99e09d1c9
Christian Blanquera <chris@incept.asia>
2021-10-23 11:31:07 +0800

    removing vfs. see: virtual_modules

--- packages/virtualfs/src/Exception.ts
+++
@@ -1,3 +0,0 @@
-import { Exception } from '@inceptjs/framework';
-
-export default class VirtualFSException extends Exception {}
--- packages/virtualfs/src/index.ts
+++
@@ -1,5 +0,0 @@
-import { Response } from '@inceptjs/framework';
-import VirtualFS, { vfs } from './VirtualFS';
-import VirtualFSWebpackPlugin from './WebpackPlugin';
-export default vfs;
-export { VirtualFS , VirtualFSWebpackPlugin, Response };
